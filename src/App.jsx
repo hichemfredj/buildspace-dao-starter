a56334d5513f1a0b82034b14ce5e81982d70d530
hichemfredj <hichemfredj123@gmail.com>
2022-02-08 20:37:20 -0500

    🤔 Check if user owns a membership NFT.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,58 @@
 /* eslint-disable no-unused-vars */
 
-import { useEffect, useMemo, useState  } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 // import thirdweb
-
 import { useWeb3 } from "@3rdweb/hooks";
+
+// import ThirdWeb SDK
+import { ThirdwebSDK } from "@3rdweb/sdk";
+
+// First we need to instanciate the sdk on Rinkeby
+const sdk = new ThirdwebSDK("rinkeby");
+
+// We need to grab a reference to our ERC-1155 contract.
+const bundleDropModule = sdk.getBundleDropModule(
+  "0x658094B52CcA4827B102eA97A7a0F4C9Ead35A32",
+);
 
 const App = () => {
   // Use the connectWallet hook thirdweb give us.
-  const {connectWallet, address, error, provider } = useWeb3();
+  const { connectWallet, address, error, provider } = useWeb3();
   console.log("👋 Address:", address);
+
+  // create a state variable to know if user has our NFT.
+  const [hasClaimNFT, setHasClaimNFT] = useState(false);
+
+  useEffect(() => {
+
+    // If we don't have an connected wallet, exit!
+    if (!address) {
+      return;
+    }
+
+    // Check if the user has the NFT by using bundleDropModule.balanceOF
+    return bundleDropModule
+      .balanceOf(address, "0")
+      .then((balance) => {
+        // If balance is greater then 0, they have our NFT!
+        if (balance.gt(0)) {
+          setHasClaimNFT(true);
+          console.log("🌟 this user has a membership NFT!")
+        } else {
+          setHasClaimNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.")
+        }
+      })
+      .catch((error) => {
+        setHasClaimNFT(false);
+        console.error("failed to NFT balance", error);
+      });
+  }, [address]);
 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
-  if(!address){
+  if (!address) {
     return (
       <div className="landing">
         <h1>Welcome to SkiSnowDAO</h1>
